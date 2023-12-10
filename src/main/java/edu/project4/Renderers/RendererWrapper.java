6d0769f29e2e9c957eb68064932db25d2ab55d27
chessplayer123 <ieriemkin20022002@gmail.com>
2023-12-10 22:19:19 +0300

    Checkstyle fix

--- src/main/java/edu/project4/Renderers/RendererWrapper.java
+++ src/main/java/edu/project4/Renderers/RendererWrapper.java
@@ -9,7 +9,6 @@ import edu.project4.Transformations.Transformation;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 public class RendererWrapper {
